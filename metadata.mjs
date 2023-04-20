e2ce634dd7601302fee6cddb6dcf90bbc39f004d
Diego Inácio <diegodci@gmail.com>
2023-04-20 10:03:47 -0300

    Update metadata with new page

--- metadata.mjs
+++ metadata.mjs
@@ -133,6 +133,12 @@ export const INDEX = [
       "Main mathematical concepts applied to <em>Computer Vision</em>.",
     notebooks: [
       {
+        id: "capital-sigma-notation",
+        name: "Capital-sigma notation",
+        description:
+          "Brief notes and practical examples with the <em>summation</em> operator.",
+      },
+      {
         id: "calculus_fourier-series",
         name: "Calculus - Fourier Series",
         description: "Brief overview of <em>Fourier series</em>.",
